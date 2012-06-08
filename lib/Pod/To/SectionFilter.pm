beac27d966208476f917dd5f9a82217a8db1a9f6
Moritz Lenz <moritz@faui2k3.org>
2012-06-08 23:40:15 +0200

    add a Pod::To:: module that searches for a given section

---
+++ lib/Pod/To/SectionFilter.pm
@@ -0,0 +1,23 @@
+use Pod::To::Text;
+class Pod::To::SectionFilter {
+    method render(@pod) {
+        my $search_for = %*ENV<PERL6_POD_HEADING> // die 'env var missing'
+        my @blocks := @pod[0].content;
+        my $from;
+        my $heading-level;
+        for @blocks.kv -> $idx, $b {
+            if $b ~~ Pod::Heading && $b.content[0].content[0] eq $search_for {
+                $from = $idx;
+                $heading-level = $b.level;
+            }
+        }
+        my $to = @blocks.end;
+        for $from + 1 .. @blocks.end -> $i {
+            if @blocks[$i] ~~ Pod::Heading && @blocks[$i].level <= $heading-level {
+                $to = $i - 1;
+                last;
+            }
+        }
+        Pod::To::Text.render(@blocks[$from..$to]);
+    }
+}
