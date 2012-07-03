13f6b55d68fc2b1ab048b089602689f16ff77afe
Moritz Lenz <moritz@faui2k3.org>
2012-07-03 15:36:16 +0200

    [htmlify] prepare for chunky Bacon, erm Pod
    this creates some infrastructure that we will need to extract
    per-routine documentation into separate output files

--- htmlify.pl
+++ htmlify.pl
@@ -4,15 +4,52 @@ use v6;
 # this script isn't in bin/ because it's not meant
 # to be installed.
 
+my %routines;
+
 sub MAIN($out_dir = 'html') {
     mkdir $out_dir unless $out_dir.IO ~~ :e;
 
     # TODO:  be recursive instead
     my @source = dir('lib').grep(*.f).grep(rx{\.pod$});
 
+    my $tempfile = join '-', "tempfile", $*PID, (1..1000).pick ~ '.temp';
+
     for (@source) {
         my $podname = .basename.subst(rx{\.pod$}, '').subst(:g, '/', '::');
         say "$_.path() => $podname";
         shell("perl6 --doc=HTML $_.path() > $out_dir/$podname.html");
+
+        # disable the rest of the processing for now, doesn't 
+        # really do anthing except burning CPU cycles
+        next;
+
+        shell("perl6 -Ilib --doc=Serialization $_.path() > $tempfile");
+        # assume just one pod block for now
+        my ($pod) = eval slurp $tempfile;
+        my @chunks = chunks-grep($pod.content,
+                :from({ $_ ~~ Pod::Heading and .level == 2}),
+                :to({ $^b ~~ Pod::Heading and $^b.level <= $^a.level}),
+            );
+        for @chunks {
+            say .perl;
+        }
+        unlink $tempfile;
+    }
+}
+
+sub chunks-grep(:$from!, :&to!, *@elems) {
+    my @current;
+
+    gather {
+        for @elems -> $c {
+            if @current && ($c ~~ $from || to(@current[0], $c)) {
+                take [@current];
+                @current = ();
+            }
+            elsif @current or $c ~~ $from {
+                @current.push: $c;
+            }
+        }
+        take [@current] if @current;
     }
 }
