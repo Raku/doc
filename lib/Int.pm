ce8f432b0570bacc192d61f20261b0c1740b7672
Moritz Lenz <moritz@faui2k3.org>
2012-07-03 15:30:19 +0200

    heading level refactor
    use =TITLE as top level heading, and then start with =head1 methods
    
    this effectively lowers the rank of most headings by one

--- lib/Int.pm
+++ lib/Int.pm
@@ -1,14 +1,14 @@
 =begin pod
 
-=head1 Int
+=TITLE class Int
 
     class Int is Cool does Real { ... }
 
 C<Int> objects store integral numbers of arbitrary size. C<Int>s are immutable.
 
-=head2 Operators
+=head1 Operators
 
-=head3 div
+=head2 div
 
     multi sub infix:<div>(Int:D, Int:D) returns Int:D
 
