da6549716937d2e3c93281963fd9a11b68139ef4
Gabor Szabo <szabgab@gmail.com>
2012-07-05 13:04:21 +0300

    index the X<> tags as well and display the whole section. Use as    p6doc '$_' and p6doc '$*OS'

--- index.pl
+++ index.pl
@@ -5,14 +5,18 @@ sub MAIN() {
 	my $outfile = "index.ini";
 	my %words;
 	for dir('lib') -> $file {
+		my $pod = substr($file.Str, 0 , $file.Str.chars -4);
 		next if $file !~~ /\.pod$/;
-		#say $file.Str;
+		my $section = '';
 		for open('lib/' ~ $file.Str).lines -> $row {
 			#if $row ~~ /^\=(item|head\d) \s+ X\<(.*)\> \s*$/ {
 			if $row ~~ /^\=(item|head\d) \s+ (.*?) \s*$/ {
-				my $w = $1.Str;
-				%words{$w}.push(substr($file.Str, 0 , $file.Str.chars -4));
-				#say '    ', $1.Str;
+				$section = $1.Str;
+				%words{$section}.push([$pod, $section]);
+			}
+			if $row ~~ /X\<(.*?)\>/ and $section {
+				my $x = $0.Str;
+				%words{$x}.push([$pod, $section]);
 			}
 		}
 	}
