202b1659e6c38b93452a29590164fc5825a0c125
Moritz Lenz <moritz@faui2k3.org>
2012-07-05 11:12:22 +0200

    write per-routine HTML files

--- htmlify.pl
+++ htmlify.pl
@@ -10,7 +10,7 @@ my %types;
 my %routines;
 
 sub MAIN($out_dir = 'html') {
-    for ('', <type language>) {
+    for ('', <type language routine>) {
         mkdir "$out_dir/$_" unless "$out_dir/$_".IO ~~ :e;
     }
 
@@ -39,11 +39,14 @@ sub MAIN($out_dir = 'html') {
             my $name = $chunk[0].content[0].content[0];
             next if $name ~~ /\s/;
             %names{$name}<routine>.push: "/type/$podname.html#$name";
-            %routines{$name}.push: $chunk;
+            %routines{$name}.push: $podname => $chunk;
         }
         unlink $tempfile;
     }
-    write-index-file($out_dir);
+    write-index-file(:$out_dir);
+    for %routines.kv -> $name, @chunks {
+        write-routine-file(:$out_dir, :$name, :@chunks);
+    }
     # TODO: write per-routine docs
     # TODO: write top-level disambiguation files
 }
@@ -109,7 +112,8 @@ sub pod-heading($name, :$level = 1) {
     );
 }
 
-sub write-index-file($out_dir) {
+sub write-index-file(:$out_dir!) {
+    say "Writing $out_dir/index.html";
     my $pod = pod-with-title('Perl 6 Documentation',
         Pod::Block::Para.new(
             content => ['Official Perl 6 documentation'],
@@ -128,3 +132,18 @@ sub write-index-file($out_dir) {
     $file.print: pod2html($pod);
     $file.close;
 }
+
+sub write-routine-file(:$name!, :$out_dir!, :@chunks!) {
+    say "Writing $out_dir/routine/$name.html";
+    my $pod = pod-with-title("Documentation for routine $name",
+        pod-block("Documentation for routine $name, assembled from the
+            following types:"));
+    $pod.content.push: @chunks.map(-> Pair (:key($type), :value($chunk)) {
+            pod-heading($type),
+            pod-block("From ", pod-link($type, "/type/$type/#$name")),
+            @$chunk
+        });
+    my $file = open :w, "$out_dir/routine/$name.html";
+    $file.print: pod2html($pod);
+    $file.close;
+} 
