05d04857bdd8daf7b24474d5107046ea8d9bef48
Moritz Lenz <moritz@faui2k3.org>
2012-08-12 21:43:20 +0200

    flesh out Documentable a bit

--- lib/Perl6/Documentable.pm
+++ lib/Perl6/Documentable.pm
@@ -1,9 +1,9 @@
+use URI::Escape;
 class Perl6::Documentable {
     has Str $.kind;     # type, language doc, routine, operator
     has Str $.subkind;  # class/role/enum, sub/method, prefix/infix/...
 
     has Str $.name;
-    has Str $.url;
     has     $.pod;
     has Bool $.pod-is-complete;
 
@@ -11,5 +11,18 @@ class Perl6::Documentable {
         $.kind eq 'operator'
             ?? "$.subkind operator"
             !! $.subkind // $.kind;
+    }
+
+    method filename() {
+        $.kind eq 'operator'
+            ?? "html/language/operators.html"
+            !! "html/$.kind/$.name.html"
+            ;
+    }
+    method url() {
+        $.kind eq 'operator'
+            ?? "/language/operator#$.subkind%20" ~ uri_escape($.name)
+            !! "/$.kind/$.name"
+            ;
     }
 }
