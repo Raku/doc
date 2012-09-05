1f37c81b2fd7fce9729d74660094e15f0e73dc95
Geoffrey Broadwell <geoff@broadwell.org>
2012-09-04 20:47:19 -0700

    Trivial Mojolicious::Lite app to serve static content from html dir; currently uses redirects instead of content negotiation

---
+++ app.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use Mojolicious::Lite;
+
+my $app = $ENV{MOJO_APP};
+$app->{static}->root('html');
+
+get '(*dir)/:file' => sub {
+    my $self = shift;
+    my $dir  = $self->param('dir');
+    my $file = $self->param('file');
+    return $self->redirect_to("/$dir/$file.html");
+};
+
+get '(*dir)/' => sub {
+    my $self = shift;
+    my $dir  = $self->param('dir');
+    return $self->redirect_to("/$dir/index.html");
+};
+
+get '/' => sub {
+    my $self = shift;
+    return $self->redirect_to('/index.html');
+};
+
+$app->start;
