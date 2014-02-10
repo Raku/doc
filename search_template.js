bc6d198b6923057ce00d0536621528a82261d6d1
raydiak <raydiak@cyberuniverses.com>
2014-02-10 22:48:57 +0000

    Slice 'n dice search for reusability

--- search_template.js
+++ search_template.js
@@ -1,14 +1,10 @@
-function display () {
-  var iframe_doc = $("#buffer").get()[0].contentDocument;
-  $("#display").html(iframe_doc.body.innerHTML);
-}
 $(function(){
   $("#query").autocomplete({
     source: [
 ITEMS
     ],
     select: function (event, ui) {
-      $("#buffer").attr("src", ui.item.url);
+	  search_select(ui.item.url);
     }
   }).focus();
 });
