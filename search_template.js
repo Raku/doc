c54f7cf5f528682753cab71b87a499c07fcef9f8
raydiak <raydiak@cyberuniverses.com>
2014-02-11 03:59:26 +0000

    Fix home search positioning and focus-stealing

--- search_template.js
+++ search_template.js
@@ -1,10 +1,15 @@
 $(function(){
-  $("#query").autocomplete({
+  var autocomplete_opts = {
+    position: search_position,
     source: [
 ITEMS
     ],
     select: function (event, ui) {
       search_select(ui.item.url);
     }
-  }).focus();
+  };
+  
+  if (search_position) autocomplete_opts.position = search_position;
+  
+  $("#query").autocomplete(autocomplete_opts);
 });
