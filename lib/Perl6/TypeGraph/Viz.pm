36a77510f60c65430dc760b8f852c47f98af5440
Mouq <alexmoquin@gmail.com>
2015-01-26 22:48:40 -0500

    Fix deprecated code warning (uniq -> unique)

--- lib/Perl6/TypeGraph/Viz.pm
+++ lib/Perl6/TypeGraph/Viz.pm
@@ -38,7 +38,8 @@ class Perl6::TypeGraph::Viz {
             @.types .= unique;
 
             # Find a new batch of seed nodes
-            @seeds = uniq(@seeds>>.sub, @seeds>>.doers);
+            @seeds = unique
+            (@seeds>>.sub, @seeds>>.doers);
 
             # If we're not growing the node pool, stop trying
             last if @.types <= @prev or !@seeds;
