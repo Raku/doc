b3e570960cd549abbab8ec8420a862f6caa73a5a
Bruce Gray <bruce.gray@acm.org>
2015-02-09 20:25:37 -0600

    Change Viz to work with Rakudo Star 2014.12

--- lib/Perl6/TypeGraph/Viz.pm
+++ lib/Perl6/TypeGraph/Viz.pm
@@ -38,8 +38,7 @@ class Perl6::TypeGraph::Viz {
             @.types .= unique;
 
             # Find a new batch of seed nodes
-            @seeds = unique
-            (@seeds>>.sub, @seeds>>.doers);
+            @seeds = (@seeds>>.sub, @seeds>>.doers).unique;
 
             # If we're not growing the node pool, stop trying
             last if @.types <= @prev or !@seeds;
