6c72b35694ac89909c7fb030d03e93595bb5cd7f
Moritz Lenz <moritz@faui2k3.org>
2015-08-22 14:56:50 +0200

    Avoid Javascript error
    search.js / search_template.js uses jquery for running JS when the
    document is ready, but cannot use jQuery for that, because jQuery is lazily
    loaded, and thus might not be available yet (#117)

--- template/search_template.js
+++ template/search_template.js
@@ -1,4 +1,6 @@
-$(function(){
+// cannot use $(...) here, because jQuery is loaded asynchronously,
+// and might not be available yet.
+document.addEventListener("DOMContentLoaded", function(event) {
   $('#search').css('visibility', 'visible');
   $("#query").autocomplete({
       position: { my: "right top", at: "right bottom", of: "#search div" },
