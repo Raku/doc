d7719ae863832042349730245bd07947437a74a4
Zoffix Znet <cpan@zoffix.com>
2015-08-22 13:12:17 -0400

    Show a "Not Found" message when nothing is found (Closes #84)

--- template/search_template.js
+++ template/search_template.js
@@ -1,6 +1,10 @@
 $(function(){
   $('#search').css('visibility', 'visible');
   $("#query").autocomplete({
+      response: function( e, ui ) {
+        if ( ! ui.content.length ) { $('#search').addClass(   'not-found') }
+        else {                       $('#search').removeClass('not-found') }
+      },
       position: { my: "right top", at: "right bottom", of: "#search div" },
       source: [
 ITEMS
