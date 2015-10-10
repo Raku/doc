b7c0c2dc131fa3c228d126171e796d98ea595275
Zoffix Znet <cpan@zoffix.com>
2015-10-09 22:27:14 -0400

    Better display of header on mobiles

---
+++ html/js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+    setup_search_box();
+    $(window).resize(setup_search_box);
+});
+
+function setup_search_box() {
+    var sel = $('#search'), head = $('#logo').parent();
+    if ( ! sel.length ) { return; }
+
+    if ( head.offset().top + head.innerHeight() <= sel.offset().top ) {
+        sel.addClass('two-row');
+    }
+    else {
+        sel.removeClass('two-row');
+    }
+}
