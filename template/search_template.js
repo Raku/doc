4a633f1ebb9dc920ec7b451e527a6f84c735388e
Zoffix Znet <cpan@zoffix.com>
2015-10-09 22:48:37 -0400

    Fix search results rendered partially off-screen on mobile (Closes #150)

--- template/search_template.js
+++ template/search_template.js
@@ -4,6 +4,12 @@ $(function(){
         if ( ! ui.content.length ) { $('#search').addClass(   'not-found') }
         else {                       $('#search').removeClass('not-found') }
       },
+      open: function() {
+        var ui_el = $('.ui-autocomplete');
+        if ( ui_el.offset().left < 0 ) {
+            ui_el.css({left: 0})
+        }
+      },
       position: { my: "right top", at: "right bottom", of: "#search div" },
       source: [
 ITEMS
