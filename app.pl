63c5009b6163d668f258170bc2fd7514001c2314
Josh Soref <jsoref@users.noreply.github.com>
2016-06-09 15:20:36 +0000

    add 404 existence guards for make-run server

--- app.pl
+++ app.pl
@@ -8,12 +8,18 @@ get '(*dir)/:file' => sub {
     my $self = shift;
     my $dir  = $self->param('dir');
     my $file = $self->param('file');
+    unless (-f "html/$dir/$file.html") {
+        return $self->render(template => 'does_not_exist');
+    }
     return $self->redirect_to("/$dir/$file.html");
 };
 
 get '(*dir)/' => sub {
     my $self = shift;
     my $dir  = $self->param('dir');
+    unless (-d "html/$dir") {
+        return $self->render(template => 'does_not_exist');
+    }
     return $self->redirect_to("/$dir.html");
 };
 
