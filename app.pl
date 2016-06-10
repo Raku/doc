9b72be06e6db0550bb7e7fe812d706c8d1d0459a
Zoffix Znet <cpan@zoffix.com>
2016-06-10 08:56:44 -0400

    Prevent infinite redirect on 404 files
    Related to #576

--- app.pl
+++ app.pl
@@ -4,16 +4,16 @@ use Mojolicious::Lite;
 
 app->static->paths(['html']);
 
-get '(*dir)/:file' => sub {
+get '*dir/:file' => sub {
     my $self = shift;
     my $dir  = $self->param('dir');
     my $file = $self->param('file');
     return $self->redirect_to("/$dir/$file.html");
 };
 
-get '(*dir)/' => sub {
+get '*dir' => [ dir => qr{.+/} ] => sub {
     my $self = shift;
-    my $dir  = $self->param('dir');
+    ( my $dir = $self->param('dir') ) =~ s{/$}{};
     return $self->redirect_to("/$dir.html");
 };
 
