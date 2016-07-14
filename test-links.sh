2842424ad9aae50feeefeb0623bc42da1831a265
Wenzel P. P. Peppmeyer <wenzel.peppmeyer@gmx.de>
2016-07-14 09:55:20 +0200

    remove anchor part, wget wont check them anyways

--- test-links.sh
+++ test-links.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+cut -d '#' -f 1 html/links.txt | sort | uniq > html/links.tmp
+mv html/links.tmp html/links.txt
 wget -O- -i html/links.txt -B https://docs.perl6.org --method=HEAD 2>&1 | perl -ne '$_ =~ s/(.+)\n$/$1 /; print $_' | grep -v '200 OK'
