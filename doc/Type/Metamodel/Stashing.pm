c6c80729438b887c33962e4f176d26368430cb78
cfa <215106+cfa@users.noreply.github.com>
2018-03-14 21:14:57 -0700

    Document a bit more of the metamodel.
    Add brief pages on Metamodel::ModuleHOW and Metamodel::PackageHOW
    and document their roles.
    
    A couple of the roles included here are stubbed; they're included
    so that the newly added pages are properly cross referenced but
    would benefit from expansion, further examples.

---
+++ doc/Type/Metamodel/Stashing.pm
@@ -0,0 +1,17 @@
+=begin pod
+
+=TITLE role Metamodel::Stashing
+
+=SUBTITLE Metaobject that supports stash creation.
+
+    role Metamodel::Stashing { }
+
+=head1 Methods
+
+=head2 method add_stash
+
+     method add_stash($type_obj)
+
+Adds a stash for the supplied type object.
+
+=end pod
