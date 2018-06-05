9120db3ceace584311fac3f346f368966a734265
Will "Coke" Coleda <will@coleda.com>
2018-06-05 16:45:12 -0400

    Don't run full test when no changes made

--- util/update-and-test.sh
+++ util/update-and-test.sh
@@ -6,5 +6,9 @@ OLD=`git rev-parse HEAD`
 git pull --rebase
 NEW=`git rev-parse HEAD`
 
-# Test only those files that have changed.
-TEST_FILES=$(git diff --name-only $OLD..$NEW | xargs) make xtest
+if [ "x$OLD" == "x$NEW" ]; then
+    echo "No changes to test.";
+else
+    # Test only those files that have changed.
+    TEST_FILES=$(git diff --name-only $OLD..$NEW | xargs) make xtest
+fi
