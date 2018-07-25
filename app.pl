77a32bf9368417d9c1697f6eb65e8cc06b6146e6
JJ Merelo <jjmerelo@gmail.com>
2018-07-25 09:50:20 +0200

    Adds comment to explain behavior, closes #2203

--- app.pl
+++ app.pl
@@ -76,7 +76,7 @@ sub gen_assets {
     );
     move $temp_css, 'html/css/app.css'
       or app->log->warn("Copying CSS failed: $!");
-    
+
     mkdir "html/js" unless -d "html/js";
     move $temp_js,  'html/js/app.js'
         or app->log->warn("Copying JS failed: $!");
