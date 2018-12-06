e55c92aacafa6cca9b322a1e031c713db2146673
JJ Merelo <jjmerelo@gmail.com>
2018-12-06 12:52:02 +0100

    Eliminates writing disambiguation files from test.
    This goes towards helping #2498, but also is a test that we could
    proceed with #2499.

--- util/travis-test.sh
+++ util/travis-test.sh
@@ -9,7 +9,7 @@ case "${BUILDENV}" in
     docker)
         docker run -t -v $TRAVIS_BUILD_DIR:/test jjmerelo/perl6-doccer
         docker run -t -v $TRAVIS_BUILD_DIR:/test --entrypoint="/bin/sh" jjmerelo/perl6-doccer -c "perl6 util/manage-page-order.p6 update"
-        docker run -t -v $TRAVIS_BUILD_DIR:/test --entrypoint="/bin/sh" jjmerelo/perl6-doccer -c "perl6 htmlify.p6 --no-highlight"
+        docker run -t -v $TRAVIS_BUILD_DIR:/test --entrypoint="/bin/sh" jjmerelo/perl6-doccer -c "perl6 htmlify.p6 --no-highlight --disambiguation=False"
     ;;
     whateverable)
       HEAD_BUILD=$(echo /tmp/whateverable/rakudo-moar/*)
@@ -19,6 +19,6 @@ case "${BUILDENV}" in
       P6_DOC_TEST_VERBOSE=1 make test
       make clean-build
       make gen-pod6-source
-      perl6 htmlify.p6 --no-highlight
+      perl6 htmlify.p6 --no-highlight --disambiguation=False
     ;;
 esac
