95f08d07f030d4496b349ba85ab3361ed906eeb4
Will Coleda <will@coleda.com>
2023-03-07 20:37:29 -0500

    Test added files also

--- util/test-modified.sh
+++ util/test-modified.sh
@@ -1,7 +1,7 @@
 # This script will run make xtest on any files in the repository that have not yet been committed
 # use before 'git commit' to ensure your commit doesn't require correction.
 
-export TEST_FILES=`git status --porcelain | grep ' M' | awk '{print $2}'`
+export TEST_FILES=`git status --porcelain | egrep '^( M|A)' | awk '{print $2}'`
 
 [ "$TEST_FILES" = "" ] && echo "nothing to test"
-[ "$TEST_FILES" != "" ] && make xtest
+[ "$TEST_FILES" != "" ] && echo $TEST_FILES && make xtest
