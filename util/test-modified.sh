e334a95d1597341564ed94390146045b32677d25
Will Coleda <will@coleda.com>
2024-11-30 17:56:12 -0500

    Fix helper script to work in-repo

--- util/test-modified.sh
+++ util/test-modified.sh
@@ -4,4 +4,4 @@
 export TEST_FILES=`git status --porcelain | grep ' M' | awk '{print $2}'`
 
 [ "$TEST_FILES" = "" ] && echo "nothing to test"
-[ "$TEST_FILES" != "" ] && make xtest
+[ "$TEST_FILES" != "" ] && RAKULIB=. make xtest
